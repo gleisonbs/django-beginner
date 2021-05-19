28dcb984e5a99e8e38720bb76cee81caf4690002
Gleison Batista Soares <gleisonbs@gmail.com>
2021-05-18 23:30:40 -0300

    chore: add deploy configs

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*y(uy@y=&p7$uu19a6lm%-#2t0xio(4ffl@h0jd@vy)&r5as4r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = []
 
@@ -123,3 +123,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
+STATIC_ROOT = 'static/'
